8ae68be642667b7d5f7da1dfcfeee61f0302b685
toxidxd <toxid13@yandex.ru>
2024-05-03 22:22:34 +0300

    fix

--- loader.py
+++ loader.py
@@ -2,6 +2,5 @@ from telebot import TeleBot
 from telebot.storage import StateMemoryStorage
 from config_data import config
 
-
 states_storage = StateMemoryStorage()
 bot = TeleBot(token=config.BOT_TOKEN, state_storage=states_storage)
